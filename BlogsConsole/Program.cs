5a91d15e20bc0cbaceb9cc0118dfb9e980d29b96
Josue <josuepereztorr@gmail.com>
2022-04-10 23:21:28 -0500

    - Driver Class Created

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using BlogsConsole.Controller;
 
 namespace BlogsConsole
 {
     class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly BloggingContext Context = new BloggingContext();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Logger.Info("Program started");
+
+            try
+            {
+                BloggingController controller = new BloggingController(Context, Logger);
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception.Message);
+            }
+            
+            Logger.Info("Program ended");
         }
     }
 }
